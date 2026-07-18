0f5eb9276b288f8d0cd62381d72478f08c362161
omch-29 <chandwaniom29@gmail.com>
2026-07-18 13:43:56 +0530

    modification in UI

--- client/src/components/SF.jsx
+++ client/src/components/SF.jsx
@@ -3,7 +3,7 @@ import styles from "./ShortenForm.module.css";
 
 const BASE = "/api";
 
-//shortenForm
+//shortenForm.jsx
 
 export default function SF({ onShortened }) {
   const [url, setUrl] = useState("");
